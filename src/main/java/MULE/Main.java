3c6229146132feb3f7ea78c1f228e9a94823f1a3
Thomas Coe <thomascoe1@gmail.com>
2015-09-08 19:51:51 -0400

    Fix FXML configuration

--- src/main/java/MULE/Main.java
+++ src/main/java/MULE/Main.java
@@ -4,15 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("../view/main.fxml"));
-        StackPane root = new StackPane();
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(Main.class.getResource("/main.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
