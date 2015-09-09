5bcc30917655f6afad536a54dc5b75a0791b4d8e
danielansher <danielansher@gmail.com>
2015-09-08 20:43:11 -0400

    Added basic main window.

--- src/main/java/MULE/Main.java
+++ src/main/java/MULE/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("/main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
