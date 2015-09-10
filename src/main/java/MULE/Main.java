b4368e5340174776a5374bbd4f866c7b17add090
Thomas Coe <thomascoe1@gmail.com>
2015-09-10 17:41:09 -0400

    Rename window title

--- src/main/java/MULE/Main.java
+++ src/main/java/MULE/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(Main.class.getResource("/main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("M.U.L.E.");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
