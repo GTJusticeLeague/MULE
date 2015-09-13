1dc002fff83a13c9cfd0a7d0a3b26e6ef8f3cfe4
Thomas Coe <thomascoe1@gmail.com>
2015-09-13 19:27:38 -0400

    Change level to difficulty, add getters for GameConfig variables

--- src/main/java/MULE/Difficulty.java
+++ src/main/java/MULE/Difficulty.java
@@ -3,6 +3,6 @@ package MULE;
 /**
  * Created by Donald on 9/13/2015.
  */
-public enum Level {
+public enum Difficulty {
     BEGINNER, STANDARD, TOURNAMENT
 }
--- src/main/java/MULE/GameConfig.java
+++ src/main/java/MULE/GameConfig.java
@@ -2,15 +2,28 @@ package MULE;
 
 /**
  * Created by Donald on 9/13/2015.
+ * 9/13/2015 - Changed level to difficulty, make variables final, add getters
  */
 public class GameConfig {
-    Level level;
-    MapType mapType;
-    int players;
+    private final Difficulty difficulty;
+    private final MapType mapType;
+    private final int players;
 
-    public GameConfig(Level level, MapType mapType, int players) {
-        this.level = level;
+    public GameConfig(Difficulty difficulty, MapType mapType, int players) {
+        this.difficulty = difficulty;
         this.mapType = mapType;
         this.players = players;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public MapType getMapType() {
+        return mapType;
+    }
+
+    public int getPlayers() {
+        return players;
     }
 }
