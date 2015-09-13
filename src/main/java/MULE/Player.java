14ea5d8a5e7ba1572ea396150d08f6dfa0ebd2c5
Thomas Coe <thomascoe1@gmail.com>
2015-09-13 19:49:08 -0400

    Change Player race/color to enums. Add getters.

--- src/main/java/MULE/Player.java
+++ src/main/java/MULE/Player.java
@@ -2,16 +2,45 @@ package MULE;
 
 /**
  * Created by Donald on 9/13/2015.
+ * 9/13/2015 - Made Race/Color enums, created getters, made variables private
  */
 public class Player {
 
-    String name;
-    String race;
-    String color;
+    public enum Race {
+        HUMAN,
+        FLAPPER,
+        BONZOID,
+        UGAITE,
+        BUZZITE
+    }
 
-    public Player (String name, String race, String color) {
+    public enum Color {
+        RED,
+        GREEN,
+        BLUE,
+        YELLOW,
+        PURPLE
+    }
+
+    private String name;
+    private Race race;
+    private Color color;
+
+    public Player (String name, Race race, Color color) {
             this.name = name;
             this.race = race;
             this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public Race getRace() {
+        return race;
     }
 }
