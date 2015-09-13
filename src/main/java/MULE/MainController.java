375e5baf23cb1010218e6dc9c7031b26f8a6f2e5
EmekeN <enkadi13@gmail.com>
2015-09-13 19:03:41 -0400

    controller bug fixes, added variable names for all of main.fxml objectts

--- src/main/java/MULE/MainController.java
+++ src/main/java/MULE/MainController.java
@@ -1,5 +1,35 @@
 package MULE;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+
 public class MainController {
+    @FXML
+    private Button GameConfigButton;
+
+    @FXML
+    private RadioButton PlayerNum_4;
+
+    @FXML
+    private RadioButton PlayerNum_3;
+
+    @FXML
+    private RadioButton PlayerNum_2;
+
+    @FXML
+    private RadioButton MapType_Standard;
+
+    @FXML
+    private RadioButton MapType_Random;
+
+    @FXML
+    private RadioButton Difficulty_Beginner;
+
+    @FXML
+    private RadioButton Difficulty_Intermediate;
+
+    @FXML
+    private RadioButton Difficulty_Advanced;
 
 }
