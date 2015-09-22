2faceb485b5a053673e47b1d3b9cdb1e44a50dc9
Thomas Coe <thomascoe1@gmail.com>
2015-09-21 22:31:52 -0400

    Update tile to show colored border when land purchased

--- src/main/java/MULE/GameScreenController.java
+++ src/main/java/MULE/GameScreenController.java
@@ -1,9 +1,11 @@
 package MULE;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -13,6 +15,7 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -28,7 +31,7 @@ public class GameScreenController {
     private AnchorPane anchorPane;
 
     @FXML
-    private  GridPane playerStatsGrid;
+    private GridPane playerStatsGrid;
 
     @FXML
     private Text playerName;
@@ -57,17 +60,14 @@ public class GameScreenController {
     @FXML
     private Button endButton;
 
-    Image mountain1 = new Image(getClass().getResourceAsStream("/mountain1_tile.png"));
-    Image mountain2 = new Image(getClass().getResourceAsStream("/mountain2_tile.png"));
-    Image mountain3 = new Image(getClass().getResourceAsStream("/mountain3_tile.png"));
-    Image plain = new Image(getClass().getResourceAsStream("/plain_tile.png"));
-    Image river = new Image(getClass().getResourceAsStream("/river_tile.png"));
-    Image town = new Image(getClass().getResourceAsStream("/town_tile.png"));
+    private final Image mountain1 = new Image(getClass().getResourceAsStream("/mountain1_tile.png"));
+    private final Image mountain2 = new Image(getClass().getResourceAsStream("/mountain2_tile.png"));
+    private final Image mountain3 = new Image(getClass().getResourceAsStream("/mountain3_tile.png"));
+    private final Image plain = new Image(getClass().getResourceAsStream("/plain_tile.png"));
+    private final Image river = new Image(getClass().getResourceAsStream("/river_tile.png"));
+    private final Image town = new Image(getClass().getResourceAsStream("/town_tile.png"));
 
-    Tile[][] board = GamePlay.GAMECONFIG.getGAMEBOARD().getTiles();
-
-
-    EventHandler<MouseEvent> purchaseHandler = new EventHandler<MouseEvent>() {
+    private final EventHandler<MouseEvent> purchaseHandler = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent event) {
             Text popup = new Text(100, 100, "Would you like to purchase this property?");
@@ -75,9 +75,8 @@ public class GameScreenController {
             Button no = new Button("No");
 
             yes.setOnAction(event1 -> {
-
-            buyLand((GridPane.getRowIndex((javafx.scene.Node) event.getSource())),
-                    (GridPane.getColumnIndex((javafx.scene.Node) event.getSource())));
+                buyLand((GridPane.getRowIndex(((javafx.scene.Node) event.getSource()).getParent())),
+                        (GridPane.getColumnIndex(((javafx.scene.Node) event.getSource()).getParent())));
 
                 Pane.getChildren().remove(popup);
                 Pane.getChildren().remove(yes);
@@ -91,6 +90,25 @@ public class GameScreenController {
             Pane.add(popup, 98, 100);
             Pane.add(yes, 98, 101);
             Pane.add(no, 99, 101);
+        }
+    };
+
+    private final EventHandler<MouseEvent> townClickHandler = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            Stage stage = (Stage) Pane.getScene().getWindow();
+            Parent root = null;
+            try {
+                // Load the town when the town is clicked
+                root = FXMLLoader.load(getClass().getResource("/town.fxml"));
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            event.consume();
         }
     };
 
@@ -111,61 +129,52 @@ public class GameScreenController {
         GamePlay.currentPlayer = GamePlay.GAMECONFIG.players[GamePlay.currentPlayer.getNumber() + 1];
     }
 
+    /**
+     * Displays the map on the screen. Adds the images to the GameScreen's GridPane
+     */
     private void buildMap() {
         anchorPane.setStyle("-fx-background-color: #83d95e;");
-        board = GamePlay.GAMECONFIG.getGAMEBOARD().getTiles();
+        Tile[][] board = GamePlay.GAMECONFIG.getGAMEBOARD().getTiles();
 
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                if (board[i][j].getTerrain() == Tile.Terrain.ONEMOUNTAIN) {
-                    ImageView mtn1 = new ImageView(mountain1);
-                    mtn1.setFitHeight(90);
-                    mtn1.setFitWidth(90);
-                    mtn1.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
-                    Pane.add(mtn1, j, i);
-                } else if (board[i][j].getTerrain() == Tile.Terrain.PLAIN) {
-                    ImageView ImagePlain = new ImageView(plain);
-                    ImagePlain.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
-                    Pane.add(ImagePlain, j, i);
-                } else if (board[i][j].getTerrain() == Tile.Terrain.RIVER) {
-                    ImageView ImageRiver = new ImageView(river);
-                    ImageRiver.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
-                    Pane.add(ImageRiver, j, i);
-                } else if (board[i][j].getTerrain() == Tile.Terrain.TWOMOUNTAIN) {
-                    ImageView mtn2 = new ImageView(mountain2);
-                    mtn2.setFitHeight(90);
-                    mtn2.setFitWidth(90);
-                    mtn2.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
-                    Pane.add(mtn2, j, i);
-                } else if (board[i][j].getTerrain() == Tile.Terrain.THREEMOUNTAIN) {
-                    ImageView mtn3 = new ImageView(mountain3);
-                    mtn3.setFitHeight(90);
-                    mtn3.setFitWidth(90);
-                    mtn3.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
-                    Pane.add(mtn3, j, i);
-                } else {
-                    //town
-                    ImageView townImage = new ImageView(town);
-                    townImage.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<javafx.scene.input.MouseEvent>() {
-                        @Override
-                        public void handle(MouseEvent event) {
-                            Stage stage = (Stage) Pane.getScene().getWindow();
-                            Parent root = null;
-                            try {
-                                // Load the town when the town is clicked
-                                root = FXMLLoader.load(getClass().getResource("/town.fxml"));
-                            } catch (java.io.IOException e) {
-                                e.printStackTrace();
-                                System.exit(-1);
-                            }
-                            Scene scene = new Scene(root);
-                            stage.setScene(scene);
-                            stage.show();
-                            event.consume();
-                        }
-                    });
-                    Pane.add(townImage, j, i);
+                HBox box = new HBox();
+                box.setMaxHeight(90);
+                box.setMaxWidth(90);
+                ImageView image = null;
+                switch (board[i][j].getTerrain()) {
+                    case ONEMOUNTAIN:
+                        image = new ImageView(mountain1);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
+                        break;
+                    case TWOMOUNTAIN:
+                        image = new ImageView(mountain2);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
+                        break;
+                    case THREEMOUNTAIN:
+                        image = new ImageView(mountain3);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
+                        break;
+                    case PLAIN:
+                        image = new ImageView(plain);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
+                        break;
+                    case RIVER:
+                        image = new ImageView(river);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, purchaseHandler);
+                        break;
+                    case TOWN:
+                        image = new ImageView(town);
+                        image.addEventHandler(MouseEvent.MOUSE_CLICKED, townClickHandler);
+                        break;
+                    default:
+                        System.err.println("ERROR: Invalid Terrain when displaying map!");
+                        System.exit(1);
                 }
+                image.setFitHeight(90);
+                image.setFitWidth(90);
+                box.getChildren().add(image);
+                Pane.add(box, j, i);
             }
         }
 
@@ -174,13 +183,10 @@ public class GameScreenController {
     private void setPlayerStats() {
         //show player stat boxes based on number of players
         if (GamePlay.GAMECONFIG.getNumPlayers() == 2) {
-
             playerStatsGrid.getChildren().remove(2);
             playerStatsGrid.getChildren().remove(2);
-
         } else if (GamePlay.GAMECONFIG.getNumPlayers() == 3) {
             playerStatsGrid.getChildren().remove(3);
-
         }
 
         Player[] players = GamePlay.GAMECONFIG.getPlayers();
@@ -214,29 +220,68 @@ public class GameScreenController {
 
             mule = (Label) playerStatGridPane.getChildren().get(7);
             mule.setText(currentPlayer.getMule() + " Mule");
-
         }
-
     }
 
-    public void UpdatePlayer(int playerNum) {
+    private void updatePlayer(int playerNum) {
         String money = "$" + GamePlay.GAMECONFIG.players[playerNum].getMoney();
         Pane playerStatsPane = ((Pane) playerStatsGrid.getChildren().get(playerNum));
         playerMoney = (Text) playerStatsPane.getChildren().get(1);
         playerMoney.setText(money);
     }
 
-    public void buyLand(int x, int y) {
-        Tile current = board[x][y];
+    private void buyLand(int x, int y) {
+        Tile current = GamePlay.GAMECONFIG.getGAMEBOARD().getTiles()[x][y];
         Random rand = new Random();
         int price = 300 + GamePlay.round * rand.nextInt(101);
 
+        if (GamePlay.currentPlayer == null) {
+            System.err.println("ERROR: Current player null!");
+            return;
+        }
         if (GamePlay.currentPlayer.getMoney() >= price && current.getOwner() == null || GamePlay.round > 3) {
             current.setOwner(GamePlay.currentPlayer);
             GamePlay.currentPlayer.setMoney(GamePlay.currentPlayer.getMoney() - price);
-            UpdatePlayer(GamePlay.currentPlayer.getNumber());
+            updatePlayer(GamePlay.currentPlayer.getNumber());
         }
-         //INSERT UPDATE MAP HERE
+        // Update the board
+        updateTile(x, y);
+    }
 
+    /**
+     * Updates a tile displayed on the screen (adds a colored border based on the owner)
+     * <p>
+     * If the tile does not have an owner, this method does nothing
+     *
+     * @param x The X position of the tile
+     * @param y The Y position of the tile
+     */
+    private void updateTile(int x, int y) {
+        // Get current tile, check what border color should be
+        Tile currentTile = GamePlay.GAMECONFIG.getGAMEBOARD().getTiles()[x][y];
+        if (currentTile.getOwner() == null) {
+            System.err.println("Tile has no owner. Likely due to land purchase failing");
+            return; // No need to update border color of tile with no owner
+        }
+        String color = currentTile.getOwner().getColor().toString();
+
+        // Get the Node that contains this tile
+        Node result = null;
+        ObservableList<Node> childrens = Pane.getChildren();
+        for (Node node : childrens) {
+            if (Pane.getRowIndex(node) == x && Pane.getColumnIndex(node) == y) {
+                result = node;
+                break;
+            }
+        }
+
+        // Add colored border to image
+        if (result instanceof HBox) {
+            result.setStyle("-fx-border-color: " + color + ";\n" +
+                    "-fx-border-width: 5;\n" +
+                    "-fx-border-style: solid;\n");
+        } else {
+            System.err.println("ERROR: Not an HBox.");
+        }
     }
 }
