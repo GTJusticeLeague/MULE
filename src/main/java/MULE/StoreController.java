3f7b06b1198a9546ae818c08209f949e071e71e6
jmbrown864 <jmbrown864@gmail.com>
2015-10-06 18:16:38 -0400

    some store functionality

--- src/main/java/MULE/StoreController.java
+++ src/main/java/MULE/StoreController.java
@@ -6,7 +6,9 @@ import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -14,39 +16,114 @@ import javafx.stage.Stage;
  * Created by danielansher on 10/4/15.
  */
 public class StoreController {
+
+    @FXML
+    private Text foodInStore;
+
+    @FXML
+    private Text energyInStore;
+
+    @FXML
+    private Text smithoreInStore;
+
+    @FXML
+    private Text crystiteInStore;
+
+    @FXML
+    private Text playerFood;
+
+    @FXML
+    private Text playerEnergy;
+
+    @FXML
+    private Text playerSmithore;
+
+    @FXML
+    private Text playerCrystite;
+
+    @FXML
+    private TextField foodBuy;
+
+    @FXML
+    private TextField energyBuy;
+
+    @FXML
+    private TextField smithoreBuy;
+
+    @FXML
+    private TextField crystiteBuy;
+
+    @FXML
+    private TextField muleBuy;
+
+    @FXML
+    private TextField foodSell;
+
+    @FXML
+    private TextField energySell;
+
+    @FXML
+    private TextField smithoreSell;
+
+    @FXML
+    private TextField crystiteSell;
+
+    @FXML
+    private Label mule;
+
+    @FXML
+    private Label foodTotal;
+
+    @FXML
+    private Label energyTotal;
+
+    @FXML
+    private Label smithoreTotal;
+
+    @FXML
+    private Label crystiteTotal;
+
+    @FXML
+    private Label muleTotal;
+
+    @FXML
+    private Label foodReceive;
+
+    @FXML
+    private Label energyReceive;
+
+    @FXML
+    private Label smithoreReceive;
+
+    @FXML
+    private Label crystiteReceive;
+
+    @FXML
+    private Button returnToTown;
+
+    @FXML
+    private Button checkout;
+
     @FXML
     private void initialize() {
-        //shows pop up
-        //different scene is displayed based on selection
+        //populate number of items in store
+        //populate number of items player has available
+        populatePlayerItems();
+        //a player cannot input more items than the store has
+        //a player cannot sell more items than they have
     }
 
-    private final EventHandler<MouseEvent> storeHandler = new EventHandler<MouseEvent> () {
-        @Override
-        public void handle(MouseEvent event) {
-            final Stage dialogStage = new Stage();
-            dialogStage.setTitle("Buy Or Sell Resources");
-            dialogStage.initModality(Modality.WINDOW_MODAL);
 
-            Label buyOrSellLabel = new Label("Would you like to buy or sell resources?");
-            buyOrSellLabel.setAlignment(Pos.CENTER);
+    private void populateStore() {
 
-            Button buy = new Button("Buy");
-            Button sell = new Button("Sell");
+    }
 
-            buy.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-
-                }
-            });
-
-            sell.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-
-                }
-            });
-        }
-    };
+    private void populatePlayerItems() {
+        Player current = GamePlay.currentPlayer;
+        playerFood.setText(current.getFood() + " Food");
+        playerEnergy.setText(current.getEnergy() + " Energy");
+        playerSmithore.setText(current.getSmithore() + " Smithore");
+        playerCrystite.setText(current.getCrystite() + " Crystite");
+    }
 }
 
